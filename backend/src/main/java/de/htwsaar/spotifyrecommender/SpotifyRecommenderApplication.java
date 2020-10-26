17cb819b20acc294b0e10c18e596506ec4b4f78c
Arthur Kelsch <akelsch@users.noreply.github.com>
2020-10-26 20:46:42 +0100

    Add backend boilerplate

---
+++ backend/src/main/java/de/htwsaar/spotifyrecommender/SpotifyRecommenderApplication.java
@@ -0,0 +1,13 @@
+package de.htwsaar.spotifyrecommender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyRecommenderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpotifyRecommenderApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/de/htwsaar/spotifyrecommender/SpotifyRecommenderApplicationTests.java
@@ -0,0 +1,13 @@
+package de.htwsaar.spotifyrecommender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyRecommenderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
