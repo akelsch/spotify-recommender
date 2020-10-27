ba3d6e64a683127fdbc5eb990692052354ec6bf1
Arthur Kelsch <akelsch@users.noreply.github.com>
2020-10-27 22:13:18 +0100

    Add Spotify OAuth configuration

--- backend/src/main/java/de/htwsaar/spotifyrecommender/GreetingHandler.java
+++ backend/src/main/java/de/htwsaar/spotifyrecommender/GreetingHandler.java
@@ -2,17 +2,22 @@ package de.htwsaar.spotifyrecommender;
 
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
-import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
+
+import java.security.Principal;
 
 @Component
 public class GreetingHandler {
 
     public Mono<ServerResponse> hello(ServerRequest request) {
-        return ServerResponse.ok()
-                .contentType(MediaType.TEXT_PLAIN)
-                .body(BodyInserters.fromValue("Hello, Spring!"));
+        return request.principal()
+                .map(Principal::getName)
+                .defaultIfEmpty("Spring")
+                .flatMap(name -> ServerResponse.ok()
+                        .contentType(MediaType.TEXT_PLAIN)
+                        .bodyValue("Hello, %s!".formatted(name))
+                );
     }
 }
