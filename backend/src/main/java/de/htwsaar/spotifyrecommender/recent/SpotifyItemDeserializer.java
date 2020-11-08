f494ab73a079240506b47d9a114842685e1335b8
Julian Quint <julianquint@gmx.net>
2020-11-08 17:02:46 +0100

    Adjust SpotifyItemDeserializer

--- backend/src/main/java/de/htwsaar/spotifyrecommender/recent/SpotifyItemDeserializer.java
+++ backend/src/main/java/de/htwsaar/spotifyrecommender/recent/SpotifyItemDeserializer.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.TreeNode;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.node.NullNode;
 
 import java.io.IOException;
 
@@ -11,12 +12,32 @@ public class SpotifyItemDeserializer extends JsonDeserializer<RecentlyPlayedItem
 
     @Override
     public RecentlyPlayedItem deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
-        // TODO implement conversion
-//        Kein Context -> Song
-//        Context "album" -> Album
-//        Context "artist" -> Artist
-//        Context "playlist" -> ???
         TreeNode treeNode = p.readValueAsTree();
-        return null;
+        RecentlyPlayedItem recentlyPlayedItem = new RecentlyPlayedItem();
+
+        TreeNode track = treeNode.get("track");
+
+        String id = track.get("uri").toString().replace("\"","");
+        recentlyPlayedItem.setId(id.substring(14));
+
+        String name = track.get("name").toString().replace("\"","");
+        recentlyPlayedItem.setName(name);
+
+        // TODO set ImageUrl
+        recentlyPlayedItem.setImageUrl("TODO");
+
+        TreeNode context = treeNode.get("context");
+        if(context instanceof NullNode) {
+            recentlyPlayedItem.setType("Song");
+        } else {
+            String type = context.get("type").toString().replace("\"","");
+            switch (type) {
+                case "album" -> recentlyPlayedItem.setType("Album");
+                case "artist" -> recentlyPlayedItem.setType("Artist");
+                case "playlist" -> recentlyPlayedItem.setType("Playlist");
+            }
+        }
+
+        return recentlyPlayedItem;
     }
 }
