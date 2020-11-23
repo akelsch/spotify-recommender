5e5ed0ffa6c328c9b87c810023a76d66518d746f
Arthur Kelsch <akelsch@users.noreply.github.com>
2020-11-23 18:43:09 +0100

    Adjust script

--- backend/psql-import.sh
+++ backend/psql-import.sh
@@ -5,12 +5,13 @@ if [[ $# -ne 1 ]]; then
   exit 2
 fi
 
-PGPASSWORD=$1 psql -U sptfyusr -d postgres -c "DELETE FROM Tracks; DELETE FROM Playlists;"
+PGPASSWORD=$1 psql -U sptfyusr -d postgres -c "DELETE FROM Tracks;"
+PGPASSWORD=$1 psql -U sptfyusr -d postgres -c "DELETE FROM Playlists;"
 
 for FILE in *.csv; do
   if [[ $FILE == playlists* ]]; then
     PGPASSWORD=$1 psql -U sptfyusr -d postgres -c "COPY Playlists FROM '$PWD/$FILE' CSV HEADER;"
   elif [[ $FILE == tracks* ]]; then
-    PGPASSWORD=$1 psql -U sptfyusr -d postgres -c "COPY Tracks(pid,track_uri,artist_uri,album_uri,pos) FROM '$PWD/$FILE' CSV HEADER;"
+    PGPASSWORD=$1 psql -U sptfyusr -d postgres -c "COPY Tracks(pid_fk,track_uri,artist_uri,album_uri,pos) FROM '$PWD/$FILE' CSV HEADER;"
   fi
 done
