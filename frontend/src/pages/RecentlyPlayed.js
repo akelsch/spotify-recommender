a9f5bbbe3fa7df5ee8704c938ee7a618e747ba80
Arthur Kelsch <akelsch@users.noreply.github.com>
2020-12-08 00:07:28 +0100

    Update on recently played page

--- frontend/src/pages/RecentlyPlayed.js
+++ frontend/src/pages/RecentlyPlayed.js
@@ -1,13 +1,26 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Heading } from 'react-bulma-components';
 
-import { selectRecentlyPlayedTracks } from '../reducers/recentlyPlayedReducer';
+import {
+  selectRecentlyPlayedTracks,
+  setRecentlyPlayedTracks,
+} from '../reducers/recentlyPlayedReducer';
+import SpotifyRecommenderApi from '../api/SpotifyRecommenderApi';
 import Layout from './layout/Layout';
 import SongItemList from '../components/SongItemList';
 
 function RecentlyPlayed() {
+  const dispatch = useDispatch();
   const recentlyPlayedTracks = useSelector(selectRecentlyPlayedTracks);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const newTracks = await SpotifyRecommenderApi.fetchRecentlyPlayedTracks();
+      dispatch(setRecentlyPlayedTracks(newTracks));
+    };
+    fetchData();
+  }, [dispatch]);
 
   return (
     <Layout>
