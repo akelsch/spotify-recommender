7794fc47d8bde400da0b6e0ff68dff9de3a602d7
Arthur Kelsch <akelsch@users.noreply.github.com>
2021-02-27 16:33:12 +0100

    Better exception handling for converting numbers

--- backend/src/main/java/de/htwsaar/spotifyrecommender/util/RequestUtils.java
+++ backend/src/main/java/de/htwsaar/spotifyrecommender/util/RequestUtils.java
@@ -2,9 +2,23 @@ package de.htwsaar.spotifyrecommender.util;
 
 import de.htwsaar.spotifyrecommender.util.exception.EnumMismatchException;
 import de.htwsaar.spotifyrecommender.util.exception.MissingRequestParameterException;
+import de.htwsaar.spotifyrecommender.util.exception.NumberMismatchException;
+import org.springframework.util.NumberUtils;
 import org.springframework.web.reactive.function.server.ServerRequest;
 
 public final class RequestUtils {
+
+    public static Long parseLong(String text) {
+        return RequestUtils.parseNumber(text, Long.class);
+    }
+
+    public static <T extends Number> T parseNumber(String text, Class<T> clazz) {
+        try {
+            return NumberUtils.parseNumber(text, clazz);
+        } catch (NumberFormatException e) {
+            throw new NumberMismatchException(text, e);
+        }
+    }
 
     public static <T extends Enum<T>> T getEnumQueryParam(ServerRequest request, String paramName, Class<T> clazz) {
         String paramValue = request.queryParam(paramName).orElseThrow(() -> new MissingRequestParameterException(paramName));
