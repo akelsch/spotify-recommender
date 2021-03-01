c23ce6e7bfe1bc4995626714ccaa34d2af62afef
Arthur Kelsch <akelsch@users.noreply.github.com>
2021-03-01 23:56:44 +0100

    Code style adjustments

--- backend/src/main/java/de/htwsaar/spotifyrecommender/recent/RecentlyPlayedHandler.java
+++ backend/src/main/java/de/htwsaar/spotifyrecommender/recent/RecentlyPlayedHandler.java
@@ -3,6 +3,7 @@ package de.htwsaar.spotifyrecommender.recent;
 import de.htwsaar.spotifyrecommender.spotify.SpotifyApi;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
@@ -14,7 +15,7 @@ public class RecentlyPlayedHandler {
     private final SpotifyApi spotifyApi;
 
     public Mono<ServerResponse> get(ServerRequest request) {
-        var queryParams = request.queryParams();
+        MultiValueMap<String, String> queryParams = request.queryParams();
         return spotifyApi.getRecentlyPlayed(queryParams)
                 .bodyToMono(RecentlyPlayedResponse.class)
                 .flatMap(response -> ServerResponse.ok().bodyValue(response));
